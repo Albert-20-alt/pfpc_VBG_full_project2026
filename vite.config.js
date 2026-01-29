10df09c07b4c557fe14fb137207c25e16a3fad19
tilene central <tilenecentrale@192.168.1.6>
2026-01-29 20:36:23 +0000

    Fix React chunk splitting for production build

--- vite.config.js
+++ vite.config.js
@@ -232,7 +232,7 @@ export default defineConfig({
 			output: {
 				manualChunks: (id) => {
 					if (id.includes('node_modules')) {
-						if (id.includes('react-dom') || id.includes('react-router')) {
+						if (id.includes('/react/') || id.includes('react-dom') || id.includes('react-router')) {
 							return 'react-vendor';
 						}
 						if (id.includes('@radix-ui')) {
